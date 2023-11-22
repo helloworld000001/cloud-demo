f41ef53792560c8111128cbc3e14d55a919061f4
root <2032461441@qq.com>
2023-11-22 15:51:44 +0800

    feat:eureka-server配置

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @auther 陈彤琳
+ * @Description $
+ * 2023/11/22 15:33
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
