0585af7e098ced10726160b7ee829aeaef2077d0
root <2032461441@qq.com>
2023-11-23 14:53:37 +0800

    feat:微服务配置拉取

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -4,7 +4,11 @@ import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -13,6 +17,17 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    /**
+     * 获取配置文件中的pattern.dateformat，如果能够成功获取到说明读取配置成功
+     */
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+
+    @GetMapping("/now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    }
 
     /**
      * 路径： /user/110
